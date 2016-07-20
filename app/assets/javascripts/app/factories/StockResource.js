d171770edb56c5a0d71c01dfa8454705a3ea1438
frankNowinski <nowinski.frank@gmail.com>
2016-07-20 18:23:30 -0400

    removed get stock resource

--- app/assets/javascripts/app/factories/StockResource.js
+++ app/assets/javascripts/app/factories/StockResource.js
@@ -2,7 +2,6 @@ function StockResource($resource){
 
   return $resource('/api/v1/stocks', {user_id: '@userId'}, {
     // query: {method: 'GET', isArray: true},
-    // get: {method: 'GET', isArray: true},
     create: {method: 'POST', isArray: true},
     delete: {method: 'DELETE', isArray: true}
   });
