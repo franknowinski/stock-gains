52c6f04e7ac39bad45de484c0498b814c0389777
frankNowinski <nowinski.frank@gmail.com>
2016-07-20 18:27:07 -0400

    change yql query to https

--- app/assets/javascripts/app/services/StockService.js
+++ app/assets/javascripts/app/services/StockService.js
@@ -1,7 +1,8 @@
 function StockService($http) {
 
   function baseUrl(ticker) {
-    return 'https://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22' + ticker + '%22%29&env=store://datatables.org/alltableswithkeys&format=json';
+    return
+    "https://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22" + ticker + "%22%29&env=store://datatables.org/alltableswithkeys&format=json";
   };
 
   function validateNewStock(ownedStocks, symbol){
