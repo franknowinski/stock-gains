c508bb003eeef7c1d766be953201f49bfb945141
frankNowinski <nowinski.frank@gmail.com>
2016-08-01 23:52:14 -0400

    revised stock collapsible menu/added add stock button

--- app/assets/javascripts/app/components/OwnedStocks.js
+++ app/assets/javascripts/app/components/OwnedStocks.js
@@ -11,33 +11,34 @@ var OwnedStocks = {
     $scope.$on('displayStock', function() {
       ctrl.showContent = true, ctrl.removeStock = false, ctrl.editStockForm = false;
     });
+    // 
+    // function formatDate(date) {
+    //   return $filter('date')(date,'yyyy-MM-dd');
+    // };
+    //
+    // function calculateBalance(s, property) {
+    //   return ctrl.stock.shares * s[property];
+    // };
+    //
+    // function assignBalance(stock) {
+    //   ctrl.stock.currentBalance = calculateBalance(stock, 'Ask');
+    //   ctrl.stock.previousBalance = calculateBalance(stock.previousDay, 'Close');
+    //   ctrl.stock.dailyReturn = ctrl.stock.currentBalance - ctrl.stock.previousBalance;
+    // };
+    //
+    // function assignPreviousDay(quotes) {
+    //   var lastTradeDate = Date.parse(ctrl.stock.LastTradeDate);
+    //   if (formatDate(lastTradeDate) == quotes.today.Date) {
+    //     ctrl.stock.previousDay = quotes.previousDay
+    //     ctrl.stock.Ask = quotes.today.Close;
+    //   } else {
+    //     ctrl.stock.previousDay = quotes.today;
+    //   }
+    // };
 
-    function formatDate(date) {
-      return $filter('date')(date,'yyyy-MM-dd');
-    };
-
-    function calculateBalance(s, property) {
-      return ctrl.stock.shares * s[property];
-    };
-
-    function assignBalance(stock) {
-      ctrl.stock.currentBalance = calculateBalance(stock, 'Ask');
-      ctrl.stock.previousBalance = calculateBalance(stock.previousDay, 'Close');
-      ctrl.stock.dailyReturn = ctrl.stock.currentBalance - ctrl.stock.previousBalance;
-    };
-
-    function assignPreviousDay(quotes) {
-      var lastTradeDate = Date.parse(ctrl.stock.LastTradeDate);
-      if (formatDate(lastTradeDate) == quotes.today.Date) {
-        ctrl.stock.previousDay = quotes.previousDay
-        ctrl.stock.Ask = quotes.today.Close;
-      } else {
-        ctrl.stock.previousDay = quotes.today;
-      }
-    };
     ctrl.editStockForm = function(){
       debugger;
-    }
+    };
   },
   controllerAs: 'portfolio'
 };
